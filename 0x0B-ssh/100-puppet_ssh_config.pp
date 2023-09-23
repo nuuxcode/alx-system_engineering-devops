c030eb5954861ac10341f7bf310603a1613ed1fb
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-23 13:42:33 +0100

    Task4 testing

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,31 +2,31 @@
 include stdlib
 
 file_line { 'ssh_host_school':
-  path   => '~/.ssh/school', # or the path to your SSH configuration file
+  path   => '~/.ssh/school',
   line   => 'Host school',
   match  => '^Host school$',
 }
 
 file_line { 'ssh_hostname_school':
-  path   => '~/.ssh/school', # or the path to your SSH configuration file
+  path   => '~/.ssh/school',
   line   => '    HostName 54.172.47.106',
   match  => '^    HostName 54.172.47.106$',
 }
 
 file_line { 'ssh_user_school':
-  path   => '~/.ssh/school', # or the path to your SSH configuration file
+  path   => '~/.ssh/school',
   line   => '    User ubuntu',
   match  => '^    User ubuntu$',
 }
 
 file_line { 'ssh_identity_file_school':
-  path   => '~/.ssh/school', # or the path to your SSH configuration file
+  path   => '~/.ssh/school',
   line   => '    IdentityFile ~/.ssh/school',
   match  => '^    IdentityFile ~/.ssh/school$',
 }
 
 file_line { 'ssh_password_authentication_school':
-  path   => '~/.ssh/school', # or the path to your SSH configuration file
+  path   => '~/.ssh/school',
   line   => '    PasswordAuthentication no',
   match  => '^    PasswordAuthentication no$',
 }
