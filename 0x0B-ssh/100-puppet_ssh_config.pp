dc660f1734cc6eff89abd69fa46f109a19d87bfd
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-23 13:42:11 +0100

    Task4 testing

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,34 @@
-# Client configuration file (w/ Puppet)
-file_line { 'Turn_off_passwd_auth':
-  path   => '~/.ssh/school',
-  line   => '    PasswordAuthentication no',
-  ensure => present,
+# Client configuration file
+include stdlib
+
+file_line { 'ssh_host_school':
+  path   => '~/.ssh/school', # or the path to your SSH configuration file
+  line   => 'Host school',
+  match  => '^Host school$',
 }
 
-file_line { 'Declare_identity_file':
-  path   => '~/.ssh/school',
-  line   => '    IdentityFile ~/.ssh/school',
-  ensure => present,
+file_line { 'ssh_hostname_school':
+  path   => '~/.ssh/school', # or the path to your SSH configuration file
+  line   => '    HostName 54.172.47.106',
+  match  => '^    HostName 54.172.47.106$',
 }
+
+file_line { 'ssh_user_school':
+  path   => '~/.ssh/school', # or the path to your SSH configuration file
+  line   => '    User ubuntu',
+  match  => '^    User ubuntu$',
+}
+
+file_line { 'ssh_identity_file_school':
+  path   => '~/.ssh/school', # or the path to your SSH configuration file
+  line   => '    IdentityFile ~/.ssh/school',
+  match  => '^    IdentityFile ~/.ssh/school$',
+}
+
+file_line { 'ssh_password_authentication_school':
+  path   => '~/.ssh/school', # or the path to your SSH configuration file
+  line   => '    PasswordAuthentication no',
+  match  => '^    PasswordAuthentication no$',
+}
+
+
