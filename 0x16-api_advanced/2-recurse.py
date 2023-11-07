60c0739be12f558220be5d76679f93e7b190d858
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-11-07 23:16:56 +0000

    test2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -15,11 +15,11 @@ def recurse(subreddit, after=None):
     all_posts = []
     if response.status_code == 200:
         data = response.json()
-        for post in data["data"]["children"]:
-            all_posts.append(post["data"]["title"])
         after = data["data"]["after"]
         if after is None:
             return all_posts
+        for post in data["data"]["children"]:
+            all_posts.append(post["data"]["title"])
         next = recurse(subreddit, after)
         all_posts.extend(next)
         return all_posts
