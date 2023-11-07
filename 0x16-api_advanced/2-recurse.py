3a5cc97bb4323d7dd42d60192abfc015f40d2170
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-11-07 22:54:58 +0000

    Task2 Recurse it!

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+this doc for module
+"""
+import requests
+
+
+def recurse(subreddit, after=None):
+    """method doc"""
+    headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+    params = {"after": after}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False,
+                            headers=headers, params=params)
+    all_posts = []
+    if response.status_code == 200:
+        data = response.json()
+        for post in data["data"]["children"]:
+            all_posts.append(post)
+        after = data["data"]["after"]
+        if after is None:
+            return all_posts
+        next = recurse(subreddit, after)
+        all_posts.extend(next)
+        return all_posts
+    else:
+        return all_posts
