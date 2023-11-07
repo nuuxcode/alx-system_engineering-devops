0b2bd75a8396d936362fdbc6db9a76cca86f761b
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-11-07 20:06:43 +0000

    Fix pep8

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -4,8 +4,8 @@
 """
 import sys
 
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
+if __name__ == "__main__":
+    number_of_subscribers = __import__("0-subs").number_of_subscribers
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,11 @@ this doc for module
 """
 import requests
 
-headers = {
-    "User-Agent": "MyCustomUserAgent/1.0",
-    "Content-Type": "application/json"
-}
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+
 
 def number_of_subscribers(subreddit):
-    """ method doc """
+    """method doc"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
