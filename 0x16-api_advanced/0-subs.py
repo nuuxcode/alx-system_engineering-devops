b4beb32235c4db60d930f9ffd5fe01007509de09
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-11-07 20:01:55 +0000

    Task0 How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+this doc for module
+"""
+import requests
+
+headers = {
+    "User-Agent": "MyCustomUserAgent/1.0",
+    "Content-Type": "application/json"
+}
+
+def number_of_subscribers(subreddit):
+    """ method doc """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
