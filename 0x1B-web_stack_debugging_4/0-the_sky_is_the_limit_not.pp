fcfb21097edb5417a895fa91c04f523a697b662f
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-11-18 22:14:13 +0100

    Task0 trying another way

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 # instal the_sky_is_the_limit_no
 exec { 'update ulimit':
   command => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 15000\"/' /etc/default/nginx",
-  path    => ['/bin', '/usr/bin'],
+  provider => 'shell',
 }
 
 -> exec { 'restart':
   command => 'service nginx restart',
-  path    => ['/bin', '/usr/bin'],
+  provider => 'shell',
 }
