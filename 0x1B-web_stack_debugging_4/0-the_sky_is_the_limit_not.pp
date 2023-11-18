387d5e22137cab7577d666cbcdc8281c273f20cb
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-11-18 22:14:37 +0100

    fix puppet lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 # instal the_sky_is_the_limit_no
 exec { 'update ulimit':
-  command => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 15000\"/' /etc/default/nginx",
+  command  => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 15000\"/' /etc/default/nginx",
   provider => 'shell',
 }
 
 -> exec { 'restart':
-  command => 'service nginx restart',
+  command  => 'service nginx restart',
   provider => 'shell',
 }
